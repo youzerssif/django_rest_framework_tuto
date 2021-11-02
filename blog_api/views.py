4f01d9c2df3df94eab6d4827fc33e718089ec95c
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2021-11-02 17:54:59 +0000

    init, create models, begin drf api

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
 
 # Create your views here.
+
+class PostList(generics.ListCreateAPIVIEW):
+    queryset = Post.postobjects.all()
+    serializer_class = PostSerializer
+    pass
+
+class PostDetail(generics.RetrieveDestroyAPIVIEW):
+    pass
+
